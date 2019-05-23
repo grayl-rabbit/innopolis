4d7665ed91976663b87ec922a5ad51a9d1c859de
grayl-rabbit <allisia.alister@gmail.com>
2019-05-23 23:13:33 +0300

    create service finished method getFiles add comments

--- src/main/java/part01/lesson05/task02/Main.java
+++ src/main/java/part01/lesson05/task02/Main.java
@@ -1,19 +1,30 @@
 package main.java.part01.lesson05.task02;
 
 
-import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import main.java.part01.lesson05.task02.service.RandomUtilService;
+import main.java.part01.lesson05.task02.service.RandomUtilServiceImpl;
 
+/**
+ * create some files with random text.
+ * These files have a random size
+ * The word consists of 1 <= n2 <= 15 letters
+ * The sentence consists of 1 <= n1 <= 15 words
+ * Words are separated by a single space.
+ * Sentence ends with (. |! |?) + " "
+ * array of words 1 <= n4 <= 1000. There is a probability that one of
+ * the words in this array is included in the following sentence
+ * @author L
+ */
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args){
 
-        RandomUtils randomUtils = new RandomUtils();
+        RandomUtilService randomUtilService = new RandomUtilServiceImpl();
 
+        int size = randomUtilService.getRandomNumberInRange(1, 1000);
+        String[] words = randomUtilService.generateWords(randomUtilService.getRandomNumberInRange(1, 1000));
 
-
-        randomUtils.getFiles("src/main/java/part01/lesson05/task01/result", 50);
+        randomUtilService.getFiles("src/main/java/part01/lesson05/task02/result",3, size, words,50);
 
     }
 
