3d1508724c1ac2025bdb7ed4761c27b6a8a10843
grayl-rabbit <allisia.alister@gmail.com>
2019-05-29 14:54:24 +0300

    less10 - work with lambda less05 - update imports

--- src/main/java/part01/lesson10/task01/Main.java
+++ src/main/java/part01/lesson10/task01/Main.java
@@ -1,25 +1,26 @@
-package main.java.part01.lesson05.task01;
+package main.java.part01.lesson10.task01;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.TreeSet;
+import java.util.List;
+import java.util.stream.Collectors;
+
 
 /**
  * read any file
  * sorting text by alphabet without double words
  * save list to the file-result
+ * work with lambda expression
  * @author L
  */
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        Path fileRead = Paths.get("src/main/java/part01/lesson05/task01/bel");
+        Path fileRead = Paths.get("src/main/java/part01/lesson10/task01/bel");
 
-        Path fileWrite = Paths.get("src/main/java/part01/lesson05/task01/result");
-
-/*Spent 78 milliseconds
+        Path fileWrite = Paths.get("src/main/java/part01/lesson10/task01/result");
 
         List<String> bel = Files.readAllLines(fileRead);
         List<String> collect = bel.stream()
@@ -27,15 +28,9 @@ public class Main {
                 .sorted()
                 .distinct()
                 .collect(Collectors.toList());
-         Files.write(fileWrite, collect);
-*/
 
-        /*Spent 62 millisecond*/
 
-        TreeSet<String> set = new TreeSet<>(String::compareToIgnoreCase);
-        set.addAll(Files.readAllLines(fileRead));
-
-        Files.write(fileWrite, set);
+        Files.write(fileWrite, collect);
 
     }
 }
